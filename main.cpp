357ec269ac1fca32758605be3ae4cff7f39318ec
VladimirHorban <horbanvladimir@gmail.com>
2019-11-11 18:16:56 +0200

    Macros

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <sys/ioctl.h>
+#include <syslog.h>
 #include <arpa/inet.h>
 #include <fcntl.h>
 #include <assert.h>
@@ -15,14 +16,22 @@
 
 void service( int aClientSocket )
 {
+    syslog( LOG_INFO, "Servise is runnig; client socket - %d", aClientSocket );
     char* buffer = new char[1024];
     recv( aClientSocket, buffer, 1024, 0 );
     send( aClientSocket, buffer, 1024, 0 );
+    syslog( LOG_INFO, "Servise was stoped; client socket - %d", aClientSocket );
     close( aClientSocket );
 }
 
 int main( int argc, char *argv[] )
 {
+    setlogmask (LOG_UPTO (LOG_NOTICE));
+
+    openlog ("SignalServerF2F", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
+
+    syslog ( LOG_NOTICE, "Program started by User %d", getuid() );
+
     int listenSocket   = 0;
     int newSocket      = 0;
     int port           = 0;
@@ -53,6 +62,7 @@ int main( int argc, char *argv[] )
     if( listenSocket == -1 )
     {
         perror( "Can not create listen socket" );
+        syslog( LOG_ERR, "Can not create listen socket" );
         exit( EXIT_FAILURE );
     }
 
@@ -60,7 +70,9 @@ int main( int argc, char *argv[] )
     if( result == -1 )
     {
         perror( "setsockopt() failed" );
+        syslog( LOG_ERR, "setsockopt() failed" );
         close( listenSocket );
+        closelog();
         exit( EXIT_FAILURE );
     }
 
@@ -68,7 +80,9 @@ int main( int argc, char *argv[] )
     if( result == -1 )
     {
         perror( "ioctl() failed" );
+        syslog( LOG_ERR, "ioctl() failed" );
         close( listenSocket );
+        closelog();
         exit( EXIT_FAILURE );
     }
 
@@ -79,12 +93,19 @@ int main( int argc, char *argv[] )
     if( bind( listenSocket, ( sockaddr* )&serverAddr, ( socklen_t )sizeof( sockaddr_in ) ) == -1 )
     {
         perror( "Can not bind listen socket" );
+        syslog( LOG_ERR, "Can not bind listen socket" );
+        closelog();
         exit( EXIT_FAILURE );
     }
 
     if( listen( listenSocket, maxConnections ) == -1 )
     {
         perror( "Can not listen" );
+        SYSLOG( LOG_ERR, "Can not listen", ... );
+        // google: c++ macro printf
+    #define SYSLOG(label,format, ...) printf((f_), __VA_ARGS__)
+
+        closelog();
         exit( EXIT_FAILURE );
     }
 
@@ -97,6 +118,8 @@ int main( int argc, char *argv[] )
         memcpy( &workingSet, &masterSet, sizeof( masterSet ) );
 
         printf( "%s \n", "Wait for select" );
+        syslog( LOG_INFO, "Wait for select" );
+
         nubmerOfSockets = select( maxSocket + 1, &workingSet, nullptr, nullptr, nullptr );
 
         ready = nubmerOfSockets;
@@ -104,6 +127,7 @@ int main( int argc, char *argv[] )
         if( nubmerOfSockets < 0 )
         {
             perror( "Select failed" );
+            syslog( LOG_ERR, "Select failed" );
             break;
         }
         else
@@ -118,35 +142,45 @@ int main( int argc, char *argv[] )
                         do
                         {
                            newSocket = accept( listenSocket, nullptr, nullptr );
+                           syslog( LOG_INFO, "New socket has accepted - %d", newSocket );
                            if ( newSocket < 0 )
                            {
-                              if (errno != EWOULDBLOCK)
+                              if ( errno != EWOULDBLOCK )
                               {
-                                 perror("  accept() failed");
+                                 perror( "accept() failed" );
+                                 syslog( LOG_ERR, "accept() failed %s %d", "socket - ", newSocket );
+                                 closelog();
                                  exit( EXIT_FAILURE );
                               }
+                              syslog( LOG_INFO, "Socket %d does not work", newSocket );
                               break;
                            }
 
                            printf( "New incoming connection - %d\n", newSocket );
+                           syslog( LOG_INFO, "New incoming connection - %d", newSocket );
                            FD_SET( newSocket, &masterSet );
 
                            if ( newSocket > maxSocket )
+                           {
                                maxSocket = newSocket;
+                               syslog( LOG_DEBUG, "New max socket is - %d", maxSocket );
+                           }
 
                         } while ( newSocket != -1 );
                     }
                     else
-                    {
-                        char* buffer = new char[1024];
-                        recv( i, buffer, 1024, 0 );
-                        send( i, buffer, 1024, 0 );
-                        close( i );
+                    {                                               
+                        std::thread newThread( service, i );
+                        newThread.detach();
+
                         FD_CLR( i, &masterSet );
                         if ( i == maxSocket )
                         {
                            while ( FD_ISSET( maxSocket, &masterSet ) == false )
-                              maxSocket -= 1;
+                           {
+                               maxSocket--;
+                               syslog( LOG_INFO, "New max socket is - %d", maxSocket );
+                           }
                         }
                     }
                 }
@@ -154,5 +188,6 @@ int main( int argc, char *argv[] )
         }
     }
 
+    closelog();
     return EXIT_SUCCESS;
 }
